5e80a56dd28e0c16b9ac5b9ce533e9e2ebca8103
Roman Pushkin <roman.pushkin@gmail.com>
2014-09-16 23:19:47 +0400

    Add index.erb

--- app.rb
+++ app.rb
@@ -12,5 +12,5 @@ class Barber < ActiveRecord::Base
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index			
 end
