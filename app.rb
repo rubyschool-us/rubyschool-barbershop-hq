5a589c5515a0a69bf7f9a2bc7e7a5c5189b64ed7
Roman Pushkin <roman.pushkin@gmail.com>
2014-09-18 22:36:44 +0400

    Add validation

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
+	validates :name, presence: true
+	validates :phone, presence: true
+	validates :datestamp, presence: true
+	validates :color, presence: true	
 end
 
 class Barber < ActiveRecord::Base
