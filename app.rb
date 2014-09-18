8fe521bdaa258126ebaa6e07108f344b07e9d044
Roman Pushkin <roman.pushkin@gmail.com>
2014-09-18 23:03:04 +0400

    Keep values on post

--- app.rb
+++ app.rb
@@ -24,16 +24,17 @@ get '/' do
 end
 
 get '/visit' do
+	@c = Client.new
 	erb :visit
 end
 
 post '/visit' do
 
-	c = Client.new params[:client]
-	if c.save
+	@c = Client.new params[:client]
+	if @c.save
 		erb "<h2>Спасибо, вы записались!</h2>"
 	else
-		@error = c.errors.full_messages.first
+		@error = @c.errors.full_messages.first
 		erb :visit
 	end
 end
