38225e704774afbd76f5fc87762975fa9d2631a2
Roman Pushkin <roman.pushkin@gmail.com>
2014-09-18 19:25:52 +0400

    Add /visit + js plugins

--- app.rb
+++ app.rb
@@ -11,7 +11,28 @@ end
 class Barber < ActiveRecord::Base
 end
 
-get '/' do
+before do
 	@barbers = Barber.all
+end
+
+get '/' do
 	erb :index			
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+
+
+	erb "<h2>Спасибо, вы записались!</h2>"
+
 end
