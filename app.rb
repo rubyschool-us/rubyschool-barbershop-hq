fe2c6cc02d0c54402240d0b1a941a71372b76869
Roman Pushkin <roman.pushkin@gmail.com>
2014-09-18 22:52:37 +0400

    Display error on failed validation

--- app.rb
+++ app.rb
@@ -30,8 +30,9 @@ end
 post '/visit' do
 
 	c = Client.new params[:client]
-	c.save
-
-	erb "<h2>Спасибо, вы записались!</h2>"
-
+	if c.save
+		erb "<h2>Спасибо, вы записались!</h2>"
+	else
+		erb "<h2>Ошибка</h2>"
+	end
 end
